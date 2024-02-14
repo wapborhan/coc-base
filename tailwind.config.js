c4292b1c75bdabce8b8fd4ec47ba9599ac018c0b
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-02-14 14:20:12 +0600

    Daisy UI Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
       backgroundImage: {
@@ -14,5 +15,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
